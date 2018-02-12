ae68a0bb0332e8e39b8f4370053398b46109b347
Andrew Street <andrew_street@live.com>
2018-02-12 12:51:39 -0700

    slots

--- slots.rb
+++ slots.rb
@@ -3,8 +3,13 @@ require 'colorize'
 require_relative 'player'
 
 class slot
-  
 
 
+  def initialize(player)
+    puts "Slot Machine is in effect"
+    puts "How much would you like to bet??"
+    @bet = gets.to_i(round(2))
+    
 
+  end
 end
